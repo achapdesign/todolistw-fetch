2b067306e5b067a2702578416f504c18323d2ca5
achapdesign <achapdesign@gmail.com>
2023-03-10 21:05:37 +0000

    get,put

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
 import gram from '/workspace/to-do-list/src/img/gram.jpg';
 
-fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+
+
+const ToDoList = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const change1 = (e) => {
+    setInputList(e.target.value);
+  };
+  const change2 = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+  useEffect(()=>{fetch('https://assets.breatheco.de/apis/fake/todos/user/andres', {
+    method: "GET",
+    body: JSON.stringify(todos),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(resp => {
+      console.log(resp.ok); // will be true if the response is successfull
+      console.log(resp.status); // the status code = 200 or code = 400 etc.
+      console.log(resp.text()); // will try return the exact result as string
+      return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+  })
+  .then(data => {
+      //here is were your code should start after the fetch finishes
+      console.log(data); //this will print on the console the exact object received from the server
+  })
+  .catch(error => {
+      //error handling
+      console.log(error);
+  });},[])
+  useEffect(()=>{
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/andres', {
       method: "PUT",
       body: JSON.stringify(todos),
       headers: {
@@ -22,19 +58,7 @@ fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
         //error handling
         console.log(error);
     });
-
-const ToDoList = () => {
-  const [inputList, setInputList] = useState("");
-  const [items, setItems] = useState([]);
-  const change1 = (e) => {
-    setInputList(e.target.value);
-  };
-  const change2 = () => {
-    setItems((oldItems) => {
-      return [...oldItems, inputList];
-    });
-    setInputList("");
-  };
+  },[])
   const deleteItem = (ind) => {
     return setItems(
       items.filter((item, i) => {
